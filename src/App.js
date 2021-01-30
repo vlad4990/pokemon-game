a010feb75f0439b27126b0a6103cc7a7d9ab7f4a
Torgovtsev Vladislav <vlad4990@gmail.com>
2021-01-30 17:19:41 +0300

    Corrected notes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import HeaderBlock from "./components/HeaderBlock";
-import Header from "./components/Header";
-import Layout from "./components/Layout";
-import Footer from "./components/Footer";
-import bg1 from "./assets/bg1.jpg"
-import bg3 from "./assets/bg3.jpg"
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
+import bg1 from './assets/bg1.jpg'
+import bg3 from './assets/bg3.jpg'
 
 const App = () => {
     return (
         <>
             <Header
-                title = "Hello"
-                descr = "description Header"
+                title = 'Hello'
+                descr = 'description Header'
             />
             <Layout
-                title = "Layout title 1"
-                descr = "Description Layout 1"
-                id = "1"
+                title = 'Layout title 1'
+                descr = 'Description Layout 1'
+                id = '1'
                 urlBg = {bg1}
             />
             <Layout
-                title = "Layout title 2"
-                descr = "Description Layout 2"
-                colorBg = "red"
-                id = "2"
+                title = 'Layout title 2'
+                descr = 'Description Layout 2'
+                colorBg = 'pink'
+                id = '2'
             />
             <Layout
-                title = "Layout title 3"
-                descr = "Description Layout 3"
-                id = "3"
+                title = 'Layout title 3'
+                descr = 'Description Layout 3'
+                id = '3'
                 urlBg = {bg3}
             />
             <Footer/>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,32 +2,24 @@ import layoutStyle from './style.module.css';
 
 const Layout = ({ id, title, descr, urlBg, colorBg }) => {
 
-    const Style = {
+    const style = {
         backgroundImage: urlBg ? `url(${urlBg})` : 'none',
-        backgroundColor: colorBg,
+        backgroundColor: colorBg ? colorBg : 'transparent',
+        backgroundColor: colorBg ? colorBg : 'transparent',
     }
 
     return (
         <section className={layoutStyle.root}
                  id={id}
-                 style={Style}
+                 style={style}
         >
             <div className={layoutStyle.wrapper}>
                 <div className={layoutStyle.title}>
-
-                    {
-                        title && <h3>
-                            {title}
-                        </h3>
-                    }
+                    {title && (<h3>{title}</h3>)}
                     <span className={layoutStyle.separator}></span>
                 </div>
                 <div className={layoutStyle.desc + ' ' + layoutStyle.full}>
-                    {
-                        descr && <p>
-                            {descr}
-                        </p>
-                    }
+                    {descr && (<p>{descr}</p>)}
                 </div>
             </div>
         </section>
